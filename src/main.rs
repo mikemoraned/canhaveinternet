977dadbda3e0e3625213cca3360a6d6600f95ca0
Mike Moran <mike@houseofmoran.com>
2019-11-12 23:23:44 +0000

    add simple http get of rust lang site

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use reqwest;
+
+fn main() -> Result<(), reqwest::Error> {
+    let status = reqwest::get("https://www.rust-lang.org")?.status();
+
+    println!("status = {:?}", status);
+
+    Ok(())
 }
