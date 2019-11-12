985440f4c1507934c7d9b283dbd235109ead7146
Mike Moran <mike@houseofmoran.com>
2019-11-12 23:26:20 +0000

    switch to ibm

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use reqwest;
 
 fn main() -> Result<(), reqwest::Error> {
-    let status = reqwest::get("https://www.rust-lang.org")?.status();
+    let status = reqwest::get("https://www.ibm.com/uk-en")?.status();
 
     println!("status = {:?}", status);
 
