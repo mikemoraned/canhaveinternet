49faa8238fdb4f7cc7ea640567265f06d5db2984
Mike Moran <mike@houseofmoran.com>
2019-11-13 00:11:56 +0000

    switch to surf and async

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,24 @@
-use reqwest;
-use std::{thread, time};
+use async_std::io;
+use async_std::prelude::*;
+use async_std::task;
+use std::time;
+use surf;
 
-fn main() -> Result<(), reqwest::Error> {
+async fn check() -> Result<(), surf::Exception> {
+    let url = "https://www.ibm.com/uk-en";
+    let response = surf::get(url).await?;
+    println!("status = {:?}", response.status());
+
+    Ok(())
+}
+
+fn main() -> io::Result<()> {
     let delay = time::Duration::from_secs(30);
 
-    loop {
-        let status = reqwest::get("https://www.ibm.com/uk-en")?.status();
-        println!("status = {:?}", status);
-        thread::sleep(delay);
-    }
+    task::block_on(async {
+        loop {
+            task::block_on(check());
+            task::sleep(delay).await;
+        }
+    })
 }
