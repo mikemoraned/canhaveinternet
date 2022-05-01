8211145a76e5adf8b624c710ad2cb97ec4e9544c
Mike Moran <mike@houseofmoran.com>
2022-05-01 15:29:57 +0100

    get something from a Speedtest JSON

--- snitch/snitch-agent/src/main.rs
+++ snitch/snitch-agent/src/main.rs
@@ -2,31 +2,59 @@ use serde::{Deserialize, Serialize};
 use serde_json::Result;
 
 #[derive(Serialize, Deserialize)]
-struct Person {
-    name: String,
-    age: u8,
-    phones: Vec<String>,
+struct Speedtest {
+    #[serde(rename = "type")]
+    test_type: String
 }
 
 fn typed_example() -> Result<()> {
-    // Some JSON input data as a &str. Maybe this comes from the user.
     let data = r#"
-        {
-            "name": "John Doe",
-            "age": 43,
-            "phones": [
-                "+44 1234567",
-                "+44 2345678"
-            ]
-        }"#;
+    {
+        "type": "result",
+        "timestamp": "2022-05-01T14:07:28Z",
+        "ping": {
+            "jitter": 4.2119999999999997,
+            "latency": 68.644000000000005
+        },
+        "download": {
+            "bandwidth": 1624264,
+            "bytes": 19185120,
+            "elapsed": 12412
+        },
+        "upload": {
+            "bandwidth": 407593,
+            "bytes": 5687760,
+            "elapsed": 14941
+        },
+        "packetLoss": 0,
+        "isp": "Plusnet",
+        "interface": {
+            "internalIp": "192.168.2.30",
+            "name": "en0",
+            "macAddr": "18:3E:EF:E3:77:BC",
+            "isVpn": false,
+            "externalIp": "212.159.124.188"
+        },
+        "server": {
+            "id": 22971,
+            "host": "speedtest.telf.uk.as61049.net",
+            "port": 8080,
+            "name": "Exascale",
+            "location": "Telford",
+            "country": "United Kingdom",
+            "ip": "185.231.218.10"
+        },
+        "result": {
+            "id": "665584f8-9895-4a9b-878b-73db433573b9",
+            "url": "https://www.speedtest.net/result/c/665584f8-9895-4a9b-878b-73db433573b9",
+            "persisted": true
+        }
+    }"#;
 
-    // Parse the string of data into a Person object. This is exactly the
-    // same function as the one that produced serde_json::Value above, but
-    // now we are asking it for a Person as output.
-    let p: Person = serde_json::from_str(data)?;
+    let t: Speedtest = serde_json::from_str(data)?;
 
     // Do things just like with any other Rust data structure.
-    println!("Please call {} at the number {}", p.name, p.phones[0]);
+    println!("Test type {}", t.test_type);
 
     Ok(())
 }
