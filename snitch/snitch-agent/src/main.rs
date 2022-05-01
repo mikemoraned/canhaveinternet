018ad781ea3ea7b1c12ab7c9dbb39aab3dd51189
Mike Moran <mike@houseofmoran.com>
2022-05-01 16:37:32 +0100

    trigger a speedtest from a call to "/metrics"

--- snitch/snitch-agent/src/main.rs
+++ snitch/snitch-agent/src/main.rs
@@ -1,4 +1,4 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
 
 mod speedtest;
 
@@ -7,11 +7,18 @@ async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
 }
 
+#[get("/metrics")]
+async fn run_speedtest() -> impl Responder {
+    let test: speedtest::Speedtest = speedtest::run_speedtest().unwrap();
+    HttpResponse::Ok().body(format!("Test type {}, for timestamp: {}, {:?}", test.test_type, test.timestamp, test.ping))
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .service(hello)
+            .service(run_speedtest)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
