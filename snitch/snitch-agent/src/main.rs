7751951e8553ee56ea402733e1565eb4dbcb8a02
Mike Moran <mike@houseofmoran.com>
2022-05-01 16:33:42 +0100

    actix hello world

--- snitch/snitch-agent/src/main.rs
+++ snitch/snitch-agent/src/main.rs
@@ -1,6 +1,22 @@
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+
 mod speedtest;
 
-fn main() {
-    let test: speedtest::Speedtest = speedtest::run_speedtest().unwrap();
-    println!("Test type {}, for timestamp: {}, {:?}", test.test_type, test.timestamp, test.ping);
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+
+    // let test: speedtest::Speedtest = speedtest::run_speedtest().unwrap();
+    // println!("Test type {}, for timestamp: {}, {:?}", test.test_type, test.timestamp, test.ping);
 }
