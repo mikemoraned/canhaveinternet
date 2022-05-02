858bd81f5b4fc56bc203d99d860c1e6865ad5da4
Mike Moran <mike@houseofmoran.com>
2022-05-02 23:05:30 +0100

    basic infludb writer

--- snitch/snitch-agent/src/main.rs
+++ snitch/snitch-agent/src/main.rs
@@ -1,74 +1,45 @@
-use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
-use prometheus_client::encoding::text::encode;
-use prometheus_client::metrics::histogram::{Histogram, linear_buckets};
-use prometheus_client::registry::Registry;
-use std::sync::Mutex;
+extern crate dotenv;
+use futures::prelude::*;
+use influxdb2::Client;
+use chrono::{DateTime, Utc};
+use influxdb2::models::DataPoint;
+
 
 mod speedtest;
 
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
-}
+// use std::time::Duration;
+// use async_std::task;
+// async fn periodically_run_speedtest(speedtest_binary: Box<String>, ping_jitter: Box<Histogram>) {
+//     loop {
+//         println!("Running test");
+//         let test: speedtest::Speedtest = speedtest::run_speedtest(&speedtest_binary).unwrap();
+//         println!("Test type {}, for timestamp: {}, {:?}", test.test_type, test.timestamp, test.ping);
+//         ping_jitter.observe(test.ping.jitter);
+//         task::sleep(Duration::from_secs(60)).await;
+//     }
+// }
 
-struct AppState {
-    registry: Mutex<Registry>,
-}
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let host= dotenv::var("INFLUXDB_HOST").unwrap();
+    let org = dotenv::var("INFLUXDB_ORG").unwrap();
+    let token = dotenv::var("INFLUXDB_TOKEN").unwrap();
+    let bucket = "snitch-agent";
+    let client = Client::new(host, org, token);
+     
+    let points = vec![
+        DataPoint::builder("cpu")
+            .tag("host", "server01")
+            .field("usage", 0.5)
+            .build()?,
+        DataPoint::builder("cpu")
+            .tag("host", "server01")
+            .tag("region", "us-west")
+            .field("usage", 0.87)
+            .build()?,
+    ];
+                                                             
+    client.write(bucket, stream::iter(points)).await?;
 
-#[get("/metrics")]
-async fn metrics(data: web::Data<AppState>) -> impl Responder {
-    let registry = &*(data.registry.lock().unwrap()); 
-    
-    let mut buffer = vec![];
-    encode(&mut buffer, registry).unwrap();
-
-    let body = String::from_utf8(buffer).unwrap();
-
-    HttpResponse::Ok().body(body)
-}
-
-use std::time::Duration;
-use async_std::task;
-async fn periodically_run_speedtest(speedtest_binary: Box<String>, ping_jitter: Box<Histogram>) {
-    loop {
-        println!("Running test");
-        let test: speedtest::Speedtest = speedtest::run_speedtest(&speedtest_binary).unwrap();
-        println!("Test type {}, for timestamp: {}, {:?}", test.test_type, test.timestamp, test.ping);
-        ping_jitter.observe(test.ping.jitter);
-        task::sleep(Duration::from_secs(60)).await;
-    }
-}
-
-use clap::{Parser};
-
-#[derive(Parser)]
-#[clap(author, version, about, long_about = None)]
-struct Cli {
-    speedtest_binary_path: String,
-}
-
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    let cli = Cli::parse();
-
-    let mut registry = <Registry>::default();
-    let ping_jitter = Histogram::new(linear_buckets(0.0, 1.0, 100));
-    registry.register("ping_jitter", "Ping Jitter", Box::new(ping_jitter.clone()));
-
-
-    let data = web::Data::new(AppState {
-        registry: Mutex::new(registry)
-    });
-
-    actix_rt::spawn(periodically_run_speedtest(Box::new(cli.speedtest_binary_path.clone()), Box::new(ping_jitter.clone())));
-
-    HttpServer::new(move|| {
-        App::new()
-            .app_data(data.clone())
-            .service(hello)
-            .service(metrics)
-    })
-    .bind(("0.0.0.0", 8080))?
-    .run()
-    .await
+    Ok(())
 }
