8ee2c740a15a95d520499410d098f7f24d9994ec
Mike Moran <mike@houseofmoran.com>
2022-05-02 23:36:34 +0100

    sending real data to influxdb

--- snitch/snitch-agent/src/main.rs
+++ snitch/snitch-agent/src/main.rs
@@ -3,43 +3,43 @@ use futures::prelude::*;
 use influxdb2::Client;
 use chrono::{DateTime, Utc};
 use influxdb2::models::DataPoint;
-
+use std::time::Duration;
+use tokio::{time, task};
 
 mod speedtest;
 
-// use std::time::Duration;
-// use async_std::task;
-// async fn periodically_run_speedtest(speedtest_binary: Box<String>, ping_jitter: Box<Histogram>) {
-//     loop {
-//         println!("Running test");
-//         let test: speedtest::Speedtest = speedtest::run_speedtest(&speedtest_binary).unwrap();
-//         println!("Test type {}, for timestamp: {}, {:?}", test.test_type, test.timestamp, test.ping);
-//         ping_jitter.observe(test.ping.jitter);
-//         task::sleep(Duration::from_secs(60)).await;
-//     }
-// }
+async fn periodically_run_speedtest(speedtest_binary: &str, client: &Client) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    loop {
+        println!("Running test");
+        let test: speedtest::Speedtest = speedtest::run_speedtest(&speedtest_binary).unwrap();
+        println!("Test type {}, for timestamp: {}, {:?}", test.test_type, test.timestamp, test.ping);
+        let bucket = "snitch-agent";
+
+        let points = vec![
+            DataPoint::builder("speedtest")
+                .tag("test_type", test.test_type)
+                .field("ping_jitter", test.ping.jitter)
+                .build()?
+            ];
+                                                             
+        client.write(bucket, stream::iter(points)).await?;
+
+        println!("Waiting ...");
+        time::sleep(Duration::from_secs(60)).await;
+    }
+}
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let host= dotenv::var("INFLUXDB_HOST").unwrap();
     let org = dotenv::var("INFLUXDB_ORG").unwrap();
     let token = dotenv::var("INFLUXDB_TOKEN").unwrap();
-    let bucket = "snitch-agent";
     let client = Client::new(host, org, token);
      
-    let points = vec![
-        DataPoint::builder("cpu")
-            .tag("host", "server01")
-            .field("usage", 0.5)
-            .build()?,
-        DataPoint::builder("cpu")
-            .tag("host", "server01")
-            .tag("region", "us-west")
-            .field("usage", 0.87)
-            .build()?,
-    ];
-                                                             
-    client.write(bucket, stream::iter(points)).await?;
+    let speedtest_binary = "/opt/homebrew/bin/speedtest";
+    task::spawn(async move {
+        periodically_run_speedtest(speedtest_binary, &client).await?;
 
-    Ok(())
+        Ok::<(), Box<dyn std::error::Error + Send + Sync>>(())
+    }).await?
 }
